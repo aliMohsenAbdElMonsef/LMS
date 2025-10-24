57b47d9ee9920426307a4bf6d11ed69e55cd6399
yara <yarahossam97591@gmail.com>
2025-10-25 02:12:57 +0300

    Create course controller

--- LMS/DataAccess/Contracts/Repositories/ILectureRepository.cs
+++ LMS/DataAccess/Contracts/Repositories/ILectureRepository.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,7 @@ namespace LMS.DataAcess.Contracts.Repositories
     public interface ILectureRepository : IBaseRepository<Lecture, string>
     {
         Task AddRangeAsync(List<Lecture> lectures);
+        Task<Lecture> FindByIdAsync(string id);
         Task<IEnumerable<Lecture>> GetCourseOcturesAsync(string courseId);
     }
 }
