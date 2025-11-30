6b1c6817eff897c156643ff812e55afa0b44ba7b
alimohsen <lymhsnbdalmnsf@gmail.com>
2025-11-30 17:23:47 +0200

    Add missing using directive for SignInAsync extension method

--- LMS/LMS.MVC/Controllers/ProfileController.cs
+++ LMS/LMS.MVC/Controllers/ProfileController.cs
@@ -1,5 +1,6 @@
 using LMS.MVC.Models.ViewModels.Profile;
 using LMS.MVC.Services.Contracts;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
